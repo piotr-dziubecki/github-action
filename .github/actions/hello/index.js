d2cb6d331b11c9aff4792ee2b5b6a9fa9536a041
duy khanh nguyen <duykhanh9242@gmail.com>
2020-08-25 10:55:48 -0700

    - action update

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -40,7 +40,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(540);
+/******/ 		return __webpack_require__(273);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -4622,6 +4622,61 @@ module.exports = function normalizeHeaderName(headers, normalizedName) {
   });
 };
 
+
+/***/ }),
+
+/***/ 273:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const GhostAdminAPI = __webpack_require__(98);
+const showdown  = __webpack_require__(872);
+const fetch = __webpack_require__(467);
+const core = __webpack_require__(186)
+const github = __webpack_require__(438)
+
+const name = core.getInput()
+const converter = new showdown.Converter()
+let htmlValue = null
+
+// // Configure the client
+// const api = new GhostAdminAPI({
+//   url: 'http://localhost:2368',
+//   // Admin API key goes here
+//   key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
+//   version: 'v2'
+// });
+
+// fetch('https://github.com/self-coding-crab/codesandbox-onmessage/blob/master/README.md') 
+//     .then(response => response.text())
+//     .then(result => {
+//       console.log('result', result)
+//       api.posts.add({
+//         title: 'Hello world', 
+//         html: result
+//       }, {source: 'html'})
+//       // .then(response => console.log(response))
+//       // .catch(error => console.error(error))
+//     });
+
+// console.log(htmlValue)
+// // Make an authenticated request
+
+try {
+  console.log('repo', github.repository, github.event.pull_request.number)
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
+// - name: get list of files in PR
+// if: github.event.pull_request.merged
+// run: |
+//     URL="https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}/files"
+//     FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename')
+//     if echo $FILES | grep -q ".json"; then
+//       echo "json file changed!!"
+//     else
+//       echo "no json file changed!!"
+//     fi
 
 /***/ }),
 
@@ -10011,63 +10066,6 @@ class RequestError extends Error {
 exports.RequestError = RequestError;
 //# sourceMappingURL=index.js.map
 
-
-/***/ }),
-
-/***/ 540:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-const GhostAdminAPI = __webpack_require__(98);
-const showdown  = __webpack_require__(872);
-const fetch = __webpack_require__(467);
-const core = __webpack_require__(186)
-const github = __webpack_require__(438)
-
-const name = core.getInput()
-const converter = new showdown.Converter()
-let htmlValue = null
-
-// Configure the client
-const api = new GhostAdminAPI({
-  url: 'http://localhost:2368',
-  // Admin API key goes here
-  key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
-  version: 'v2'
-});
-
-fetch('https://github.com/self-coding-crab/codesandbox-onmessage/blob/master/README.md') 
-    .then(response => response.text())
-    .then(result => {
-      console.log('result', result)
-      api.posts.add({
-        title: 'Hello world', 
-        html: result
-      }, {source: 'html'})
-      // .then(response => console.log(response))
-      // .catch(error => console.error(error))
-    });
-
-console.log(htmlValue)
-// Make an authenticated request
-
-(async function main() {
-  try {
-    console.log('repo', github.repository, github.event.pull_request.number)
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-})
-// - name: get list of files in PR
-// if: github.event.pull_request.merged
-// run: |
-//     URL="https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}/files"
-//     FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename')
-//     if echo $FILES | grep -q ".json"; then
-//       echo "json file changed!!"
-//     else
-//       echo "no json file changed!!"
-//     fi
 
 /***/ }),
 
--- .github/actions/index.js
+++ .github/actions/index.js
@@ -8,37 +8,35 @@ const name = core.getInput()
 const converter = new showdown.Converter()
 let htmlValue = null
 
-// Configure the client
-const api = new GhostAdminAPI({
-  url: 'http://localhost:2368',
-  // Admin API key goes here
-  key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
-  version: 'v2'
-});
+// // Configure the client
+// const api = new GhostAdminAPI({
+//   url: 'http://localhost:2368',
+//   // Admin API key goes here
+//   key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
+//   version: 'v2'
+// });
 
-fetch('https://github.com/self-coding-crab/codesandbox-onmessage/blob/master/README.md') 
-    .then(response => response.text())
-    .then(result => {
-      console.log('result', result)
-      api.posts.add({
-        title: 'Hello world', 
-        html: result
-      }, {source: 'html'})
-      // .then(response => console.log(response))
-      // .catch(error => console.error(error))
-    });
+// fetch('https://github.com/self-coding-crab/codesandbox-onmessage/blob/master/README.md') 
+//     .then(response => response.text())
+//     .then(result => {
+//       console.log('result', result)
+//       api.posts.add({
+//         title: 'Hello world', 
+//         html: result
+//       }, {source: 'html'})
+//       // .then(response => console.log(response))
+//       // .catch(error => console.error(error))
+//     });
 
-console.log(htmlValue)
-// Make an authenticated request
+// console.log(htmlValue)
+// // Make an authenticated request
 
-(async function main() {
-  try {
-    console.log('repo', github.repository, github.event.pull_request.number)
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-})
+try {
+  console.log('repo', github.repository, github.event.pull_request.number)
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 // - name: get list of files in PR
 // if: github.event.pull_request.merged
 // run: |
