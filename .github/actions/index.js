7658a5b5000a2de673eec2a347be9c3e2bf23f24
duy khanh nguyen <duykhanh9242@gmail.com>
2020-08-25 12:15:59 -0700

    - build with zeit ncc

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,36 +1,35 @@
 const GhostAdminAPI = require('@tryghost/admin-api');
-// const showdown  = require('showdown');
+const showdown  = require('showdown');
 const fetch = require("node-fetch");
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-const name = core.getInput()
-// const converter = new showdown.Converter()
+const converter = new showdown.Converter()
 let htmlValue = null
 
 // console.log(htmlValue)
 // // Make an authenticated request
 
 try {
+  const api = new GhostAdminAPI({
+    url: 'http://localhost:2368',
+    // Admin API key goes here
+    key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
+    version: 'v2'
+  });
+
   fetch(github.context.payload.pull_request.url+'/files')
     .then(response => response.json())
     .then(files => {
       files.map(file => {
         // Configure the client
-        const api = new GhostAdminAPI({
-          url: 'http://localhost:2368',
-          // Admin API key goes here
-          key: '5f3e801b7d2ab73c67113ec2:f9a4c5835daacfb432ce34c62efd5baec5d009819c2d5badc5ab3962715fb5f4',
-          version: 'v2'
-        });
-
         fetch(file.raw_url) 
             .then(response => response.text())
             .then(result => {
               console.log('result', result)
               api.posts.add({
                 title: 'Hello world', 
-                html: result
+                html: converter.makeHtml(result)
               }, {source: 'html'})
               // .then(response => console.log(response))
               // .catch(error => console.error(error))
