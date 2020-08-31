1b86e13c61043a4b63a12985b98df76abc3667c3
duy khanh nguyen <duykhanh9242@gmail.com>
2020-08-31 08:50:41 -0700

    - rebuild with new api key

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,41 +1,45 @@
 const GhostAdminAPI = require('@tryghost/admin-api');
+const showdown  = require('showdown');
 const fetch = require("node-fetch");
 const core = require("@actions/core")
 const github = require("@actions/github")
 
+const converter = new showdown.Converter()
 let htmlValue = null
 
-// Configure the client
-const api = new GhostAdminAPI({
-  url: 'https://blog-pr.casperlabs.io',
-  // Admin API key goes here
-  key: '5f492151f94320000142e86f:3a504ff9996824e0e5311113afe686541380e20290b9b4acad61f1de7403a771',
-  version: 'v2'
-});
+// console.log(htmlValue)
+// // Make an authenticated request
 
-fetch('https://github.com/self-coding-crab/codesandbox-onmessage/blob/master/README.md') 
-    .then(response => response.text())
-    .then(result => {
-      console.log('result', result)
-      api.posts.add({
-        title: 'Hello world', 
-        html: result
-      }, {source: 'html'})
-      // .then(response => console.log(response))
-      // .catch(error => console.error(error))
-    });
+try {
+  const api = new GhostAdminAPI({
+    url: 'https://blog-pr.casperlabs.io',
+    // Admin API key goes here
+    key: '5f492151f94320000142e86f:3a504ff9996824e0e5311113afe686541380e20290b9b4acad61f1de7403a771',
+    version: 'v2'
+  });
 
-console.log(htmlValue)
-// Make an authenticated request
-
-(async function main() {
-  try {
-    console.log('repo', github.repository, github.event.pull_request.number)
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-})
+  fetch(github.context.payload.pull_request.url+'/files')
+    .then(response => response.json())
+    .then(files => {
+      files.map(file => {
+        // Configure the client
+        fetch(file.raw_url) 
+            .then(response => response.text())
+            .then(result => {
+              console.log('result', result)
+              api.posts.add({
+                title: 'Hello world', 
+                html: converter.makeHtml(result)
+              }, {source: 'html'})
+              // .then(response => console.log(response))
+              // .catch(error => console.error(error))
+            });
+      })
+    })
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 // - name: get list of files in PR
 // if: github.event.pull_request.merged
 // run: |
